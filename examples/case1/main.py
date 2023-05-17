91d6d79fbf705c7ac1df7e3f62aa6f4812913aae
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-05-17 09:54:58 +0200

    update case1

---
+++ examples/case1/main.py
@@ -0,0 +1,161 @@
+import numpy as np
+import porepy as pp
+import scipy.sparse as sps
+
+from data import Data
+from problem import Problem
+
+import sys; sys.path.insert(0, "../../src/")
+from flow import Flow
+import tags
+from exporter import write_network_pvd, make_file_name
+
+# ------------------------------------------------------------------------------#
+
+def main(region):
+
+    # tolerance in the computation
+    tol = 1e-10
+
+    # assign the flag for the low permeable fractures
+    epsilon = 1e-2
+
+    file_name = "case1"
+    if region == None:
+        folder_name = "./sol/adaptative/"
+    elif region == "region":
+        folder_name = "./sol/heterogeneous/"
+    elif region == "region_darcy":
+        folder_name = "./sol/darcy/"
+    elif region == "region_forsh":
+        folder_name = "./sol/forsh/"
+
+    variable_to_export = [Flow.pressure, Flow.P0_flux, Flow.permeability, Flow.P0_flux_norm, Flow.region, Flow.gradient_pressure]
+
+    max_iteration_non_linear = 40
+    max_err_non_linear = 1e-8
+
+    # create the grid bucket
+    problem = Problem()
+    problem.read_perm()
+
+    # create the discretization
+    discr = Flow(problem.mdg, discr = pp.MVEM)
+    test_data = Data(problem, epsilon=epsilon, region=region)
+
+    for sd, d in problem.mdg.subdomains(return_data=True):
+        d.update({pp.STATE: {}})
+        flux = np.zeros((3, sd.num_cells))
+        d[pp.STATE].update({Flow.P0_flux: flux})
+        d[pp.STATE].update({Flow.P0_flux + "_old": flux})
+
+    variable_to_export += problem.save_perm()
+
+    # non-linear problem solution with a fixed point strategy
+    err_non_linear = max_err_non_linear + 1
+    iteration_non_linear = 0
+    while err_non_linear > max_err_non_linear and iteration_non_linear < max_iteration_non_linear:
+
+        # solve the linearized problem
+        discr.set_data(test_data.get())
+
+        A, b = discr.matrix_rhs()
+        x = sps.linalg.spsolve(A, b)
+        discr.extract(x, u_bar=1)
+
+        # compute the exit condition
+        all_flux = np.empty((3, 0))
+        all_flux_old = np.empty((3, 0))
+        all_cell_volumes = np.empty(0)
+        for sd, d in problem.mdg.subdomains(return_data=True):
+            # collect the current flux
+            flux = d[pp.STATE][Flow.P0_flux]
+            all_flux = np.hstack((all_flux, flux))
+            # collect the old flux
+            flux_old = d[pp.STATE][Flow.P0_flux + "_old"]
+            all_flux_old = np.hstack((all_flux_old, flux_old))
+            # collect the cell volumes
+            all_cell_volumes = np.hstack((all_cell_volumes, sd.cell_volumes))
+            # save the old flux
+            d[pp.STATE][Flow.P0_flux + "_old"] = flux
+
+        # compute the error and normalize the result
+        err_non_linear = np.sum(all_cell_volumes * np.linalg.norm(all_flux - all_flux_old, axis=0))
+        norm_flux_old = np.sum(all_cell_volumes * np.linalg.norm(all_flux_old, axis=0))
+        err_non_linear = err_non_linear / norm_flux_old if norm_flux_old != 0 else err_non_linear
+
+        # exporter
+        save = pp.Exporter(problem.mdg, "sol_" + file_name, folder_name=folder_name)
+        save.write_vtu(variable_to_export, time_step=iteration_non_linear)
+
+        print("iteration non-linear problem", iteration_non_linear, "error", err_non_linear)
+        iteration_non_linear += 1
+
+    save.write_pvd(np.arange(iteration_non_linear))
+    write_network_pvd(file_name, folder_name, np.arange(iteration_non_linear))
+
+    for sd, d in problem.mdg.subdomains(return_data=True):
+        if region is None:
+            np.savetxt(Flow.region, d[pp.STATE][Flow.region])
+        flux = d[pp.STATE][Flow.P0_flux]
+        pressure = d[pp.STATE][Flow.pressure]
+
+    return flux, pressure, problem.mdg
+
+# ------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    print("Perform the adaptative scheme")
+    q_adapt, p_adapt, mdg = main(None)
+    print("Perform the heterogeneous scheme")
+    q_hete, p_hete, _ = main("region")
+    print("Perform the darcy-based scheme")
+    q_darcy, p_darcy, _ = main("region_darcy")
+    print("Perform the forshheimer-based scheme")
+    q_forsh, p_forsh, _ = main("region_forsh")
+
+    region = np.loadtxt("region").astype(bool)
+    p_ref = p_forsh
+    q_ref = q_forsh
+
+    for reg in np.unique(region):
+        pos = region == reg
+        # mass matrix
+        mass = sps.diags([sd.cell_volumes[pos] for sd in mdg.subdomains()], [0])
+
+        norm_scalar = lambda x: np.sqrt(x @ mass @ x)
+        norm_vector = lambda x: np.sqrt(np.linalg.norm(x, axis=0) @ mass @ np.linalg.norm(x, axis=0))
+
+        # we assume to be the adaptative solution as the reference
+        norm_p_ref = norm_scalar(p_ref[pos])
+        norm_q_ref = norm_vector(q_ref[:, pos])
+
+        # let's compute the errors
+        err_p_hete = norm_scalar(p_ref[pos] - p_hete[pos]) / norm_p_ref
+        err_q_hete = norm_vector(q_ref[:, pos] - q_hete[:, pos]) / norm_q_ref
+
+        err_p_darcy = norm_scalar(p_ref[pos] - p_darcy[pos]) / norm_p_ref
+        err_q_darcy = norm_vector(q_ref[:, pos] - q_darcy[:, pos]) / norm_q_ref
+
+        err_p_forsh = norm_scalar(p_ref[pos] - p_forsh[pos]) / norm_p_ref
+        err_q_forsh = norm_vector(q_ref[:, pos] - q_forsh[:, pos]) / norm_q_ref
+
+        print("Region", {True: "darcy", False: "forsh"}[reg])
+        print("------")
+
+        print("Errors for case hete:")
+        print("pressure", err_p_hete)
+        print("flux", err_q_hete)
+        print("------")
+
+        print("Errors for case darcy:")
+        print("pressure", err_p_darcy)
+        print("flux", err_q_darcy)
+        print("------")
+
+        print("Errors for case forsh:")
+        print("pressure", err_p_forsh)
+        print("flux", err_q_forsh)
+        print("------")
+
+    import pdb; pdb.set_trace()
--- examples/case1/spe10.py
+++ examples/case1/spe10.py
@@ -4,9 +4,9 @@ import porepy as pp
 
 # ------------------------------------------------------------------------------#
 
-class Spe10(object):
 
-# ------------------------------------------------------------------------------#
+class Spe10(object):
+    # ------------------------------------------------------------------------------#
 
     def __init__(self, layers, pos_x=None, pos_y=None):
         self.full_shape = np.array([60, 220, 85])
@@ -35,17 +35,26 @@ class Spe10(object):
         self.layers_id = None
         self.partition = None
 
-# ------------------------------------------------------------------------------#
+    # ------------------------------------------------------------------------------#
 
     def _compute_size(self):
         dim = self.layers.size
 
-        x_range = np.linspace(0, self.full_physdims[0], self.full_shape[0]+1)[np.r_[self.pos_x, self.pos_x[-1]+1]]
-        y_range = np.linspace(0, self.full_physdims[1], self.full_shape[1]+1)[np.r_[self.pos_y, self.pos_y[-1]+1]]
+        x_range = np.linspace(0, self.full_physdims[0], self.full_shape[0] + 1)[
+            np.r_[self.pos_x, self.pos_x[-1] + 1]
+        ]
+        y_range = np.linspace(0, self.full_physdims[1], self.full_shape[1] + 1)[
+            np.r_[self.pos_y, self.pos_y[-1] + 1]
+        ]
 
-        bounding_box = {"xmin": x_range[0], "xmax": x_range[-1],
-                        "ymin": y_range[0], "ymax": y_range[-1],
-                        "zmin": 0, "zmax": 0}
+        bounding_box = {
+            "xmin": x_range[0],
+            "xmax": x_range[-1],
+            "ymin": y_range[0],
+            "ymax": y_range[-1],
+            "zmin": 0,
+            "zmax": 0,
+        }
         if dim == 1:
             self.shape = np.hstack((self.pos_x.size, self.pos_y.size))
             self.physdims = np.hstack((x_range[-1], y_range[-1]))
@@ -59,26 +68,27 @@ class Spe10(object):
         self.n = np.prod(self.shape[:2])
         self.domain = pp.Domain(bounding_box=bounding_box)
 
-# ------------------------------------------------------------------------------#
+    # ------------------------------------------------------------------------------#
 
-    def _create_mdg(self,):
+    def _create_mdg(
+        self,
+    ):
         sd = pp.CartGrid(self.shape, self.domain.bounding_box)
         sd.compute_geometry()
 
         # it's only one grid but the solver is build on a mdg
         self.mdg = pp.meshing.subdomains_to_mdg([sd])
 
-# ------------------------------------------------------------------------------#
+    # ------------------------------------------------------------------------------#
 
     def read_perm(self, perm_folder):
-
         shape = (self.n, self.layers.size)
         perm_xx, perm_yy, perm_zz = np.empty(shape), np.empty(shape), np.empty(shape)
         layers_id = np.empty(shape)
 
         for pos, layer in enumerate(self.layers):
             perm_file = perm_folder + str(layer) + ".tar.gz"
-            #perm_file = perm_folder + "small_0.csv"
+            # perm_file = perm_folder + "small_0.csv"
             perm_layer = np.loadtxt(perm_file, delimiter=",")
 
             perm_x = perm_layer[:, 0].reshape(self.full_shape[:2], order="F")
@@ -90,7 +100,7 @@ class Spe10(object):
             perm_zz[:, pos] = perm_z[self.pos_x, :][:, self.pos_y].flatten(order="F")
             layers_id[:, pos] = layer
 
-        shape = self.n*self.layers.size
+        shape = self.n * self.layers.size
         perm_xx = perm_xx.reshape(shape, order="F")
         perm_yy = perm_yy.reshape(shape, order="F")
         perm_zz = perm_zz.reshape(shape, order="F")
@@ -98,31 +108,37 @@ class Spe10(object):
 
         self.layers_id = layers_id.reshape(shape, order="F")
 
-# ------------------------------------------------------------------------------#
+    # ------------------------------------------------------------------------------#
 
     def save_perm(self):
-
-        names = ["log10_perm_xx", "log10_perm_yy", "log10_perm_zz", "layer_id",
-                 "perm_xx", "perm_yy", "perm_zz"]
+        names = [
+            "log10_perm_xx",
+            "log10_perm_yy",
+            "log10_perm_zz",
+            "layer_id",
+            "perm_xx",
+            "perm_yy",
+            "perm_zz",
+        ]
 
         # for visualization export the perm and layer id
         for _, d in self.mdg.subdomains(return_data=True):
+            pp.set_solution_values(names[0], np.log10(self.perm[:, 0]), d, 0)
+            pp.set_solution_values(names[1], np.log10(self.perm[:, 1]), d, 0)
+            pp.set_solution_values(names[2], np.log10(self.perm[:, 2]), d, 0)
 
-            d[pp.STATE][names[0]] = np.log10(self.perm[:, 0])
-            d[pp.STATE][names[1]] = np.log10(self.perm[:, 1])
-            d[pp.STATE][names[2]] = np.log10(self.perm[:, 2])
+            pp.set_solution_values(names[3], self.layers_id, d, 0)
 
-            d[pp.STATE][names[3]] = self.layers_id
-
-            d[pp.STATE][names[4]] = self.perm[:, 0] * pp.DARCY
-            d[pp.STATE][names[5]] = self.perm[:, 1] * pp.DARCY
-            d[pp.STATE][names[6]] = self.perm[:, 2] * pp.DARCY
+            pp.set_solution_values(names[4], self.perm[:, 0] * pp.DARCY, d, 0)
+            pp.set_solution_values(names[5], self.perm[:, 1] * pp.DARCY, d, 0)
+            pp.set_solution_values(names[6], self.perm[:, 2] * pp.DARCY, d, 0)
 
         return names
 
-# ------------------------------------------------------------------------------#
+    # ------------------------------------------------------------------------------#
 
     def perm_as_dict(self):
         return {"kxx": self.perm[:, 0], "kyy": self.perm[:, 1], "kzz": self.perm[:, 2]}
+
 
 # ------------------------------------------------------------------------------#
