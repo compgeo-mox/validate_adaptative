c400a1805ec495d2863fe9c8cc6ce4a2f009be1d
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2023-05-17 09:55:10 +0200

    update case4

--- examples/case4/data.py
+++ examples/case4/data.py
@@ -2,12 +2,13 @@ import numpy as np
 import porepy as pp
 from functools import partial
 
-import sys; sys.path.insert(0, "../../src/")
-import tags
+import sys
+
+sys.path.insert(0, "src/")
 from permeability_convolve import *
 
-class Data:
 
+class Data:
     def __init__(self, spe10, epsilon=None, u_bar=None, region=None, tol=1e-6):
         self.spe10 = spe10
         self.tol = tol
@@ -22,24 +23,28 @@ class Data:
         u_bar = 1e-7
         epsilon = 1e-2
 
-        mu = 0.001 # fluid's viscosity [Pa.s]
-        rho = 1000. # fluid's density [kg/m3]
-        cf = 0.55 # Forchheimer coefficient [-]
-        K = 8e3 # characteristic permeability [m2]
+        mu = 0.001  # fluid's viscosity [Pa.s]
+        rho = 1000.0  # fluid's density [kg/m3]
+        cf = 0.55  # Forchheimer coefficient [-]
+        K = 8e3  # characteristic permeability [m2]
 
         # Darcy
         lambda_1 = mu
-        beta_1 = 0 *u_bar
-        phi_1 = lambda a: lambda_1 + beta_1*np.sqrt(np.abs(a))
-        pphi_1 = lambda a: lambda_1*a + 2/3*beta_1*np.power(np.abs(a), 1.5) # primitive of phi_1
+        beta_1 = 0 * u_bar
+        phi_1 = lambda a: lambda_1 + beta_1 * np.sqrt(np.abs(a))
+        pphi_1 = lambda a: lambda_1 * a + 2 / 3 * beta_1 * np.power(
+            np.abs(a), 1.5
+        )  # primitive of phi_1
         Phi_1 = lambda a: pphi_1(a)
         range_1 = lambda a: np.logical_and(a >= 0, a <= 1)
 
         # Forsh
         lambda_2 = mu
-        beta_2 = cf*rho*np.sqrt(K) *u_bar
-        phi_2 = lambda a: lambda_2 + beta_2*np.sqrt(np.abs(a))
-        pphi_2 = lambda a: lambda_2*a + 2/3*beta_2*np.power(np.abs(a), 1.5) # primitive of phi_2
+        beta_2 = cf * rho * np.sqrt(K) * u_bar
+        phi_2 = lambda a: lambda_2 + beta_2 * np.sqrt(np.abs(a))
+        pphi_2 = lambda a: lambda_2 * a + 2 / 3 * beta_2 * np.power(
+            np.abs(a), 1.5
+        )  # primitive of phi_2
         Phi_2 = lambda a: pphi_2(a) + pphi_1(1) - pphi_2(1)
         range_2 = lambda a: a > 1
 
@@ -50,22 +55,24 @@ class Data:
         self.k_ref = compute_permeability(epsilon, ranges, phi=phi, Phi=Phi)
 
         self.k_adapt = lambda flux2: self.k_ref(flux2) / u_bar
-        self.k_darcy = lambda _: 1/lambda_1 / u_bar
-        self.k_forsh = lambda flux2: 1/phi_2(flux2) / u_bar
+        self.k_darcy = lambda _: 1 / lambda_1 / u_bar
+        self.k_forsh = lambda flux2: 1 / phi_2(flux2) / u_bar
 
         # well data
         self.well_radius = 5 * pp.CENTIMETER
-        self.well_perm = 1000 # TO FIX THIS PARAMETER
+        self.well_perm = 1000  # TO FIX THIS PARAMETER
 
     # ------------------------------------------------------------------------------#
 
     def get(self):
-        return {"k": self.perm,
-                "bc": self.bc,
-                "source": self.source,
-                "vector_source": self.vector_source,
-                "well_radius": self.well_radius,
-                "tol": self.tol}
+        return {
+            "k": self.perm,
+            "bc": self.bc,
+            "source": self.source,
+            "vector_source": self.vector_source,
+            "well_radius": self.well_radius,
+            "tol": self.tol,
+        }
 
     # ------------------------------------------------------------------------------#
 
@@ -75,10 +82,15 @@ class Data:
             return np.zeros(sd.num_cells)
 
         if sd.dim == 1 and sd.well_num > -1:
-            return np.pi * np.square(self.well_radius) * np.ones(sd.num_cells) * self.well_perm
+            return (
+                np.pi
+                * np.square(self.well_radius)
+                * np.ones(sd.num_cells)
+                * self.well_perm
+            )
 
         # cell flux
-        flux = d[pp.STATE][flow_solver.P0_flux]
+        flux = d[pp.TIME_STEP_SOLUTIONS][flow_solver.P0_flux][0]
         flux_norm2 = np.square(np.linalg.norm(flux, axis=0))
 
         if self.region is not None:
@@ -102,22 +114,20 @@ class Data:
     # ------------------------------------------------------------------------------#
 
     def vector_source(self, sd, d, flow_solver):
-
         if sd.dim == 0:
             return np.zeros((sd.num_cells, 3))
 
         perm = self.perm(sd, d, flow_solver)
         coeff = 0 * sd.cell_volumes.copy() / perm
 
-        vect = np.vstack(
-                (coeff, np.zeros(sd.num_cells), np.zeros(sd.num_cells))
-                ).ravel(order="F")
+        vect = np.vstack((coeff, np.zeros(sd.num_cells), np.zeros(sd.num_cells))).ravel(
+            order="F"
+        )
         return vect
 
     # ------------------------------------------------------------------------------#
 
     def bc(self, sd, data, tol, flow_solver):
-
         # define the labels and values for the boundary faces
         bc_val = np.zeros(sd.num_faces)
 
--- examples/case4/main.py
+++ examples/case4/main.py
@@ -3,16 +3,20 @@ import porepy as pp
 import scipy.sparse as sps
 
 from data import Data
-import sys; sys.path.insert(0, "../case1/")
+import sys
+
+sys.path.insert(0, "examples/case1/")
 from spe10 import Spe10
 
-import sys; sys.path.insert(0, "../../src/")
+import sys
+
+sys.path.insert(0, "src/")
 from flow import Flow
-import tags
 from exporter import write_network_pvd, make_file_name
 from solver_codim2 import MVEMCodim2
 
 # ------------------------------------------------------------------------------#
+
 
 def add_wells(mdg, domain, well_coords, well_num_cells, tol):
     # define the wells
@@ -23,7 +27,7 @@ def add_wells(mdg, domain, well_coords, well_num_cells, tol):
     mesh_size = np.amin(wells_length / well_num_cells)
 
     # create the wells grids
-    wells_set = pp.WellNetwork3d(wells, domain, parameters={"mesh_size": mesh_size})
+    wells_set = pp.WellNetwork3d(domain, wells, parameters={"mesh_size": mesh_size})
 
     # add the wells to the MixedDimensionalGrid
     wells_set.mesh(mdg)
@@ -34,7 +38,7 @@ def add_wells(mdg, domain, well_coords, well_num_cells, tol):
 
     # select only some of the nodes that are on the surface
     selected_nodes = sd.nodes[2, :] >= np.amin(well_coords[:, 2]) - tol
-    cells = np.flatnonzero(sd_cell_nodes.T*selected_nodes)
+    cells = np.flatnonzero(sd_cell_nodes.T * selected_nodes)
 
     # compute the intersection between the wells and the grid
     pp.fracs.wells_3d.compute_well_rock_matrix_intersections(mdg, cells, tol=tol)
@@ -43,58 +47,73 @@ def add_wells(mdg, domain, well_coords, well_num_cells, tol):
 
 
 def main(region):
-
     # tolerance in the computation
     tol = 1e-10
 
     # assign the flag for the low permeable fractures
     epsilon = 1e-2
-    u_bar = 1e-7 # 1 0.5 0.25 0.125 0.0625 0.03125
+    u_bar = 1e-7  # 1 0.5 0.25 0.125 0.0625 0.03125
 
     file_name = "case4"
-    folder_name = "./fixed/"
-    variable_to_export = [Flow.pressure, Flow.P0_flux, Flow.permeability, Flow.P0_flux_norm, Flow.region]
+    folder_name = "examples/case4/fixed/"
+    variable_to_export = [
+        Flow.pressure,
+        Flow.P0_flux,
+        Flow.permeability,
+        Flow.P0_flux_norm,
+        Flow.region,
+    ]
 
     max_iteration_non_linear = 20
     max_err_non_linear = 1e-4
 
     # create the grid bucket
-    spe10 = Spe10([35, 36, 37, 38, 39, 40])#, np.arange(10), np.arange(10))
-    spe10.read_perm("../case1/spe10_perm/")
+    spe10 = Spe10([35, 36, 37, 38, 39, 40])  # , np.arange(10), np.arange(10))
+    spe10.read_perm("examples/case1/spe10_perm/")
 
     hx, hy, hz = spe10.physdims / spe10.shape
 
     # add the wells
-    well_coords = np.asarray([
-        np.array([[1.5*hx, 1.5*hx],
-                  [1.5*hy, 1.5*hy],
-                  [spe10.physdims[2], spe10.physdims[2] - 2.5*hz]]),
-
-        np.array([[spe10.physdims[0] - 1.5*hx, spe10.physdims[0] - 1.5*hx],
-                  [spe10.physdims[1] - 1.5*hy, spe10.physdims[1] - 1.5*hy],
-                  [spe10.physdims[2], spe10.physdims[2] - 2.5*hz]]),
-    ])
+    well_coords = np.asarray(
+        [
+            np.array(
+                [
+                    [1.5 * hx, 1.5 * hx],
+                    [1.5 * hy, 1.5 * hy],
+                    [spe10.physdims[2], spe10.physdims[2] - 2.5 * hz],
+                ]
+            ),
+            np.array(
+                [
+                    [spe10.physdims[0] - 1.5 * hx, spe10.physdims[0] - 1.5 * hx],
+                    [spe10.physdims[1] - 1.5 * hy, spe10.physdims[1] - 1.5 * hy],
+                    [spe10.physdims[2], spe10.physdims[2] - 2.5 * hz],
+                ]
+            ),
+        ]
+    )
     well_num_cells = 4
 
     spe10.mdg = add_wells(spe10.mdg, spe10.domain, well_coords, well_num_cells, tol)
 
     # create the discretization
-    discr = Flow(spe10.mdg, discr = MVEMCodim2)
+    discr = Flow(spe10.mdg, discr=MVEMCodim2)
     test_data = Data(spe10, epsilon, u_bar, region=region)
 
     for sd, d in spe10.mdg.subdomains(return_data=True):
-        d.update({pp.STATE: {}})
         flux = np.zeros((3, sd.num_cells))
-        d[pp.STATE].update({Flow.P0_flux: flux})
-        d[pp.STATE].update({Flow.P0_flux + "_old": flux})
+        pp.set_solution_values(Flow.P0_flux, flux, d, 0)
+        pp.set_solution_values(Flow.P0_flux + "_old", flux.copy(), d, 0)
 
     variable_to_export += spe10.save_perm()
 
     # non-linear problem solution with a fixed point strategy
     err_non_linear = max_err_non_linear + 1
     iteration_non_linear = 0
-    while err_non_linear > max_err_non_linear and iteration_non_linear < max_iteration_non_linear:
-
+    while (
+        err_non_linear > max_err_non_linear
+        and iteration_non_linear < max_iteration_non_linear
+    ):
         # solve the linearized problem
         discr.set_data(test_data.get())
 
@@ -108,26 +127,35 @@ def main(region):
         all_cell_volumes = np.empty(0)
         for sd, d in spe10.mdg.subdomains(return_data=True):
             # collect the current flux
-            flux = d[pp.STATE][Flow.P0_flux]
+            flux = d[pp.TIME_STEP_SOLUTIONS][Flow.P0_flux][0]
             all_flux = np.hstack((all_flux, flux))
             # collect the old flux
-            flux_old = d[pp.STATE][Flow.P0_flux + "_old"]
+            flux_old = d[pp.TIME_STEP_SOLUTIONS][Flow.P0_flux + "_old"][0]
             all_flux_old = np.hstack((all_flux_old, flux_old))
             # collect the cell volumes
             all_cell_volumes = np.hstack((all_cell_volumes, sd.cell_volumes))
             # save the old flux
-            d[pp.STATE][Flow.P0_flux + "_old"] = flux
+            pp.set_solution_values(Flow.P0_flux + "_old", flux, d, 0)
 
         # compute the error and normalize the result
-        err_non_linear = np.sum(all_cell_volumes * np.linalg.norm(all_flux - all_flux_old, axis=0))
+        err_non_linear = np.sum(
+            all_cell_volumes * np.linalg.norm(all_flux - all_flux_old, axis=0)
+        )
         norm_flux_old = np.sum(all_cell_volumes * np.linalg.norm(all_flux_old, axis=0))
-        err_non_linear = err_non_linear / norm_flux_old if norm_flux_old != 0 else err_non_linear
+        err_non_linear = (
+            err_non_linear / norm_flux_old if norm_flux_old != 0 else err_non_linear
+        )
 
         # exporter
         save = pp.Exporter(spe10.mdg, "sol_" + file_name, folder_name=folder_name)
         save.write_vtu(variable_to_export, time_step=iteration_non_linear)
 
-        print("iteration non-linear problem", iteration_non_linear, "error", err_non_linear)
+        print(
+            "iteration non-linear problem",
+            iteration_non_linear,
+            "error",
+            err_non_linear,
+        )
         iteration_non_linear += 1
 
     save.write_pvd(np.arange(iteration_non_linear))
@@ -135,11 +163,12 @@ def main(region):
 
     for sd, d in spe10.mdg.subdomains(return_data=True):
         if region is None:
-            np.savetxt(Flow.region, d[pp.STATE][Flow.region])
-        flux = d[pp.STATE][Flow.P0_flux]
-        pressure = d[pp.STATE][Flow.pressure]
+            np.savetxt(Flow.region, d[pp.TIME_STEP_SOLUTIONS][Flow.region][0])
+        flux = d[pp.TIME_STEP_SOLUTIONS][Flow.P0_flux][0]
+        pressure = d[pp.TIME_STEP_SOLUTIONS][Flow.pressure][0]
 
     return flux, pressure
+
 
 # ------------------------------------------------------------------------------#
 
@@ -151,33 +180,6 @@ if __name__ == "__main__":
     # print("Perform the heterogeneous scheme")
     # q_hete, p_hete = main("region")
     print("Perform the darcy-based scheme")
-    q_darcy, p_darcy = main("region_darcy")
+    q_darcy, p_darcy = main("examples/case4/region_darcy")
     # print("Perform the forshheimer-based scheme")
     # q_forsh, p_forsh = main("region_forsh")
-
-    #import pdb; pdb.set_trace()
-
-    #for sd, data in spe10.mdg.subdomains(return_data=True):
-    #    param = {"ambient_dimension": spe10.mdg.dim_max(),
-    #             "source": np.zeros(sd.num_cells)}
-
-    #    b_faces = sd.tags['domain_boundary_faces'].nonzero()[0]
-    #    bc_type = ["neu"]*b_faces.size
-
-    #    bc_values = np.zeros(sd.num_faces)
-    #    if sd.dim == 3:
-    #        perm = spe10.perm[:, 0] * pp.DARCY
-    #    elif sd.dim == 1 and sd.well_num > -1:
-    #        top_face = np.argmax(sd.face_centers[2, :])
-    #        if sd.well_num == 0:
-    #            bc_values[top_face] = - 10 ##### set a value
-    #        else:
-    #            bc_values[top_face]  = 10 ##### set a value
-
-    #        param["well_radius"] = 10 * pp.CENTIMETER
-    #        perm = np.pi * np.square(param["well_radius"]) #### * self.electrode_sigma #####
-
-    #    param["second_order_tensor"] = pp.SecondOrderTensor(perm*np.ones(sd.num_cells))
-    #    param["bc"] = pp.BoundaryCondition(sd, b_faces, bc_type)
-    #    param["bc_values"] = bc_values
-    #    pp.initialize_data(sd, data, key, param)
